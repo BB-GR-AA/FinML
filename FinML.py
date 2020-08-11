1405e8b7784f717f12e6cc94ffc132e6682bea31
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-08-10 22:30:56 -0700

    Add API_Key as parameter in GetHistoricalData_AV function.

--- FinML.py
+++ FinML.py
@@ -1,12 +1,11 @@
 '''
 Module with useful tools.
 '''
-
+import os
 from alpha_vantage.timeseries import TimeSeries
 
-def GetHistoricalData_AV(symbol='IBM'):
+def GetHistoricalData_AV(API_Key, symbol='IBM'):
     '''Historical stock data as a pandas DataFrame. '''
-    API_Key = '8FF8W12C1V5Y69N9'
     ts = TimeSeries(key=API_Key,output_format='pandas')
     data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
     data.columns = ['open','high','low','close','volume']
--- Forecasting/StocksLinearRegression.py
+++ Forecasting/StocksLinearRegression.py
@@ -3,10 +3,10 @@ Forecasting of stock market using linear regression implemented with Pytorch.
 Visualization of the training process with TensorBoard.
 '''
 
+import os
 import sys
 sys.path.append('../')
 import FinML
 
-stock = FinML.GetHistoricalData_AV(symbol='IBM')
-
-
+API_key = open(os.path.relpath("../AVAPIK.txt"), "r").readline().rstrip('\n')
+stock = FinML.GetHistoricalData_AV(API_key, symbol='IBM')
