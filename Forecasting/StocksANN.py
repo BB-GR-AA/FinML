4d7705a1504c8f97892fc93da46f7168b168629e
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-08-19 14:21:04 -0700

    Define problem in Stock ANN.

--- Forecasting/StocksANN.py
+++ Forecasting/StocksANN.py
@@ -17,18 +17,13 @@ file_name = '../Data/ANET_2014-06-06_2020-08-18.csv'
 stock = pd.read_csv(file_name, index_col=0, usecols='close')
 close = FinML.standardize(stock['close'])      
 
-# # Sliding window of size n and output of size 1, the n+1-th sample.
-# forecast_out = round(stock.shape[0] * 0.005)   # In days. Some % of the total data.
-# forecast = stock['close'].shift(-forecast_out) # Predicted values.
-
-
+# # Split into train and test data.
+# # Sliding window of size n and output of size k+1, start with k=0.
 # # Class with simple ann architecture - torch.nn to define architecture and learn python classes.
+# # Read a little on how to chose number of layers and neurons perlayer.
 # # Train/test model - collab/Jax and tensorboard.
 # # save model and import it.
 # # Plot of predicted and oserved data.
-
-
-
-
-
+# # Possible future ideas: CNN and Hankel matrix (FFT?) to predict k+1 outputs.
+# # Possible future idea: more difficoult: candle stick chart image and output buy/sell
 
