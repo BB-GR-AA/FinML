9a7c6903b4272be7f13586af9256c2c73b817fde
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-09-19 16:19:03 -0700

    Clean up, update to-do list and add resources to StocksANN.py

--- Forecasting/StocksANN.py
+++ Forecasting/StocksANN.py
@@ -7,52 +7,93 @@ Diagrams: inkscape.
 Summary: Jupyter Notebook (theory/method, citations and key findings)
 
 To do:
+- make sata set
+- data loader https://medium.com/noumena/how-does-dataloader-work-in-pytorch-8c363a8ee6c1    
+- training function
+- https://www.youtube.com/watch?v=Jy4wM2X21u0&list=PLhhyoLH6IjfxeoooqP9rhU3HJIAVAJ3Vz&index=4&t=571s&ab_channel=AladdinPersson
 - Overview of Classes in Python https://docs.python.org/3/tutorial/classes.html
+- Watch a youtube video on classes
 - Read on choice of number of layers and neurons. Simplest for now (one hidden layer).
 - Shuffle vs alternate for time series/stocks (read and cite)
 - Read Adam optimizer
-- Useful tutorials: https://www.youtube.com/playlist?list=PLhhyoLH6IjfxeoooqP9rhU3HJIAVAJ3Vz
+- finish useful tutorials: https://www.youtube.com/playlist?list=PLhhyoLH6IjfxeoooqP9rhU3HJIAVAJ3Vz
 '''
 
+import torch # Package with data structures for tensors and their mathematical operations.
+import torch.nn as nn # Package for building and training neural networks.
+import torch.nn.functional as F # All the functions from the torch.nn library (ie activations, conv, etc).
+import torch.optim # Optimization algorithms
+from torch.utils.data import Dataset, DataLoader # For easier data set management.
 import pandas as pd
 import numpy as np
 import sys
 sys.path.append('../')
 import FinML
-import torch # Entire library
-import torch.nn as nn # All the neural network modules
-import torch.optim # Optimization algorithms
-from torch.utils.data import Dataset, DataLoader # For easier data set management (min batches, etc)
-  
 
+  
 
 ### Model architecture ###
 
 class ANN(nn.Module):
     # Inherit from nn.Module, the base class for all nn models.
     # All custom models will be a subclass of this class.
-    def __init__(self, input_size, forecast_size): # Define the initialization for our model.
+    def __init__(self, input_size, H, forecast_size): # Define the initialization for our model (layers).
         super(ANN,self).__init__() # Call initialization method of parent class. Super gives access to parent class.
-        self.fc1 = nn.Linear(input_size, 50) # Applies linear transofrmation, bias True by default.
-        self.fc2 = nn.Linear(50, forecast_size) # Output layer.
+        self.fc1 = nn.Linear(input_size, H) # Applies linear transofrmation, bias True by default.
+        self.fc2 = nn.Linear(H, forecast_size) # Output layer.
     
-    def forward(self,x): # x Is the data on which the linear transformation acts.
-        x = nn.functional.relu(self.fc1(x)) # Linear and non-linear transformations in the first layer.
+    def forward(self,x): # Specify the layers' connections.
+        # x Is the data on which the linear transformation acts.
+        x = F.relu(self.fc1(x)) # Linear and non-linear transformations in the first layer.
         x = self.fc2(x) # Linear transformations in the output layer.
         # No compeling reason to use activation function in output layer. Better to chose a suitable loss function.
         return x
 
 
+### Training function ### (move to FinML.py)
 
-### Device and hyperparameters ###
+# for epoch in range(num_epoch):
+#     # Loop over all batches in the training loader.
+#     for batch_idx, (features, targets) in enumerate(train_loader): # Get more familiar witht his line.
+#         inputs, labels = data # get the inputs
+#         inputs, labels = Variable(inputs), Variable(labels) # wrap them in Variable
+#         print(epoch, i, "inputs", inputs.data, "labels", labels.data) # Run training process.
 
-device = torch.device('cpu') # Can change to gpu if available.
-in_size = 21 # Number of days on which the prediction is based (features).
-out_size = 1 # Number of days to be predicted.
-learning_rate = 0.001 # step size
-batch_size = 32 # https://stats.stackexchange.com/questions/164876/tradeoff-batch-size-vs-number-of-iterations-to-train-a-neural-network
-num_epoch = 1 # 1 epoch means the network has seen the complete data set (Seen all batches).
+# zip() creaters an iterator of tuples,  the i-th tuple being (x_i, y_i).
+# enumerate() creates an iterator of tuples,  the i-th tuple being (counter_i, item_i). 
 
+# def train(Y, X, model, optimizer, criterion, epochs=1000):
+#     error = [] # loss at a given epoch
+#     for epoch in range(epochs):
+#         total=0 # loss for entire data set for one epoch.
+#         # Do one full fwd and bckwd pass on entire data set.
+#         for y, x in zip(Y, X):
+#             loss = criterion(model(x), y)
+#             loss.backward()
+#             optimizer.step()
+#             optimizer.zero_grad()
+#             #cumulative loss 
+#             total+=loss.item() 
+#         error.append(total)
+#         # Tensorboard
+#     return error
+
+
+# def train(model,criterion,train_loader,optimizer,epochs=5, display=True):
+#     cost = []
+#     for epoch in range(epochs):
+#         total = 0
+#         for batch_idx, (y,x) in enumerate(train_loader):
+#             if display : 
+#               print('epoch {}, batch_idx {} , batch len {}'.format(epoch, batch_idx, len(y)))
+#             loss = criterion(model(x),y)
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+#             total += loss.item()
+#         cost.append(total)
+#     return cost             
+        
 
 
 ### Load data, pre-processing and train/test sets ###
@@ -77,6 +118,12 @@ num_epoch = 1 # 1 epoch means the network has seen the complete data set (Seen a
     
 
 # Custom Dataset class for DataLoader
+
+# All custom data sets that are fed into the class DataLoader must:
+# interith the Dataset class ... but why?    
+# constructor __init__()
+# __getitem__()
+# __len__() 
 
 class StockDataset(Dataset): # Training/testing sets as parameters. Move to FinML.
     def __init__(self):
@@ -110,6 +157,9 @@ features, targets = first_data_train
 print(features, targets)
 
 # DataLoader
+
+# What exactly does DataLoader return?
+# I think it returns an iterator of tuples (x,y) in batches.
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=False, num_workers=1) # Iterable.
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, num_workers=1) # Iterable.
 
@@ -117,28 +167,25 @@ dataiter = iter(train_loader)
 data = dataiter.next()
 features, labels = data
 print(features, targets)
+# See if we can print bacth_size, iterations (number of batches).
 
-
-
-### Train/test model ###
-
-# To do: def train(Din,Dout,model,dataLoader,optimizer,loss,epochs) and add to FinML.py
-
-# model = ANN(in_size,out_size).to(device)
-# criterion = nn.MSELoss()
-# optimizer = optim.Adam(model.parameters(), lr=learning_rate) 
-
-# Training cycle.
 # total_samples = lena(dataset)
 # n_iter = math.ceil(total_samples/batch_size)
 # print(total_samples, n_iter)
 
-# for epoch in range(num_epoch):
-#     # Loop over all batches in the training loader.
-#     for batch_idx, (features, targets) in enumerate(train_loader): # Get more familiar witht his line.
-#         inputs, labels = data # get the inputs
-#         inputs, labels = Variable(inputs), Variable(labels) # wrap them in Variable
-#         print(epoch, i, "inputs", inputs.data, "labels", labels.data) # Run training process.
+### Trainining/testing ###
+
+# device = torch.device('cpu') # Can change to gpu if available.
+# in_size = 7 # Number of days on which the prediction is based (features).
+# H = 10 size of hidden layer
+# out_size = 1 # Number of days to be predicted.
+# learning_rate = 0.001 # step size
+# batch_size = 32 # https://stats.stackexchange.com/questions/164876/tradeoff-batch-size-vs-number-of-iterations-to-train-a-neural-network
+# num_epoch = 1 # 1 epoch means the network has seen the complete data set (Seen all batches).
+
+# model = ANN(in_size,out_size).to(device)
+# criterion = nn.MSELoss()
+# optimizer = optim.Adam(model.parameters(), lr=learning_rate) 
 
 
 # # save model and import it.
